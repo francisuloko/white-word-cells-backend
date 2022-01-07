e00534ca8e37509ad55df2f68a458e45a799d0da
francisuloko <francisuloko@yahoo.com>
2022-01-08 00:11:07 +0100

    Write api version 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :cells
+  has_many :cells, -> { order(created_at: :desc) }
 
+  validates :name, :email, :password, presence: true
   validates :email, uniqueness: { case_sensitive: false }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :cells
-  post 'authenticate', to: 'authentication#authenticate'
-  post 'signup', to: 'registrations#create'
+
+  namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      resources :cells
+
+      post 'authenticate', to: 'authentication#authenticate'
+      post 'signup', to: 'registrations#create'
+    end
+  end
 end
