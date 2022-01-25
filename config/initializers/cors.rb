9be0ede389af2593c02256b196652fa882293288
francisuloko <francisuloko@yahoo.com>
2022-01-26 00:00:44 +0100

    Modify cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,7 +14,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://whitewordcells.com'
+    origins 'https://www.whitewordcells.com'
 
     resource '*',
       headers: :any,
