50f583cb41f464e6342c4f22ce6cf01b89a4570d
francisuloko <francisuloko@yahoo.com>
2022-01-29 05:48:27 +0100

    Add more sites to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -21,4 +21,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
+
+  allow do
+    origins 'https://whitewordcells.netlify.app'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
 end
