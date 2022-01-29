22c555f5ab8e3906ee325029fbee71a5200f53a3
francisuloko <francisuloko@yahoo.com>
2022-01-29 21:49:13 +0100

    Remove comments from cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000'
