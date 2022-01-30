85ab7755534ceb6e3c0a84a6ae2d8f1c7b042216
francisuloko <francisuloko@yahoo.com>
2022-01-30 19:14:20 +0100

    Revert cors changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,28 +1,28 @@
-Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
-  allow do
-    origins 'https://whitewordcells.netlify.app'
+# Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+#   allow do
+#     origins 'https://whitewordcells.netlify.app'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#       credentials: true
+#   end
   
-  allow do
-    origins 'http://localhost:3000'
+#   allow do
+#     origins 'http://localhost:3000'
     
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#       credentials: true
+#   end
 
-  allow do
-    origins 'https://whitewordcells.com'
+#   allow do
+#     origins 'https://whitewordcells.com'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
-end
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#       credentials: true
+#   end
+# end
